924c4ddc7b2c114a27b9e9ef2af5e9562290ebbf
Tariq Omar <tariqomar2004@gmail.com>
2025-05-25 11:26:01 +0300

    create context class for entities

---
+++ RestaurantReservationSystem/RestaurantReservation.Db/Contexts/RestaurantReservationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Entities;
+
+namespace RestaurantReservation.Db.Contexts;
+
+public class RestaurantReservationDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Table> Tables { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
+
+
+    public RestaurantReservationDbContext(DbContextOptions<RestaurantReservationDbContext> options)
+        : base(options)
+    {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Your_Connection_String_Here");
+    }
+}
